3c253d40ef48c44388b39a41cd04551414094d7b
DVSA <dvsa@dvsa.gov.uk>
2015-06-30 09:42:19 +0100

    Direct Debit Hist Slots Column Should Be Null

--- mot-api/db/dev/update/9999-current/2015-06-25-SPMS-295-Slots-Data-Type-Change-History_Table[story].sql
+++ mot-api/db/dev/update/9999-current/2015-06-25-SPMS-295-Slots-Data-Type-Change-History_Table[story].sql
@@ -3,4 +3,4 @@
 -- changeset peleodiase:20150625100020
 
 -- Uncomment this SQL when the process of updating history table is implemented
--- ALTER TABLE `direct_debit_hist` CHANGE `slots` `slots` int(10) unsigned NOT NULL;
+-- ALTER TABLE `direct_debit_hist` CHANGE `slots` `slots` int(10) unsigned NULL;
