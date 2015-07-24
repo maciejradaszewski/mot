bfd0e9782b5b1f39ab2b6d803e9830526f7eea33
DVSA <dvsa@dvsa.gov.uk>
2015-07-24 16:05:09 +0100

    VM-2166: DVSA Administration: Create AE - Update UI; VM-10385: DVSA Administration: Create AE - Confirmation Page

--- mot-api/db/dev/releases/release-1.13.0/2015-06-24-VM-2166-create-ae-change-ui-STORY.sql
+++ mot-api/db/dev/releases/release-1.13.0/2015-06-24-VM-2166-create-ae-change-ui-STORY.sql
@@ -21,8 +21,8 @@ WHERE
             code = 'DISPLAY-DVSA-ADMIN-BOX'
     );
 
-SET @user = (SELECT id FROM person WHERE username = 'static data');
+SET @createdBy = (SELECT id FROM person WHERE user_reference = 'Static Data' OR username = 'static data');
 
 --  Simon (PO) said need to change it (same to MOT1).
-UPDATE company_type SET name='Company', last_updated_by=@user, version=version+1 WHERE code = 'RC';       --  before Registered Company
-UPDATE company_type SET name='Public Body', last_updated_by=@user, version=version+1 WHERE code = 'PA';   --  before Public Authority
+UPDATE company_type SET name='Company', last_updated_by=@createdBy, version=version+1 WHERE code = 'RC';       --  before Registered Company
+UPDATE company_type SET name='Public Body', last_updated_by=@createdBy, version=version+1 WHERE code = 'PA';   --  before Public Authority
