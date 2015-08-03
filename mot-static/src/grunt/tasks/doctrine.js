32e7d0eb29018eac5f2c8c873dba01f9e809f86e
DVSA <dvsa@dvsa.gov.uk>
2015-08-03 11:29:00 +0100

    Bugfix Grunt Doctrine Proxy Command

--- mot-static/src/grunt/tasks/doctrine.js
+++ mot-static/src/grunt/tasks/doctrine.js
@@ -8,6 +8,6 @@ module.exports = function(grunt, config) {
     if (config.environment === config.ENV_DEVELOPMENT) {
         grunt.registerTask('doctrine:default', 'Reset the local doctrine file to default settings', ['sshexec:doctrine_default_local_dist', 'sshexec:apache_restart']);
         grunt.registerTask('doctrine:optimise', 'Reset the local doctrine file to default settings', ['sshexec:doctrine_optimised_local_dist', 'sshexec:apache_restart']);
-        grunt.registerTask('doctrine:proxy', 'Generates doctrine proxies', ['sshexec:doctrine_proxy_gen']);
+        grunt.registerTask('doctrine:proxy', 'Generates doctrine proxies', ['sshexec:delete_doctrine_cache_folders', 'sshexec:doctrine_proxy_gen']);
     }
 };
