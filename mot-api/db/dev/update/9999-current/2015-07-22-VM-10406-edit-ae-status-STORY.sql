4aff25f562ea4aa974e0414946693596b5ceeac2
DVSA <dvsa@dvsa.gov.uk>
2015-08-07 18:40:46 +0100

    Sql Mods For Rel 1 14 0

--- mot-api/db/dev/update/9999-current/2015-07-22-VM-10406-edit-ae-status-STORY.sql
+++ mot-api/db/dev/update/9999-current/2015-07-22-VM-10406-edit-ae-status-STORY.sql
@@ -1,18 +1,19 @@
 SET @createdBy = (SELECT id FROM person WHERE user_reference = 'Static Data' OR username = 'static data');
 SET @displayOrder = (SELECT MAX(`display_order`) FROM `event_type_lookup`);
 
-INSERT INTO `event_type_lookup` (`code`, `name`, `display_order`, `start_date`, `end_date`, `mot1_legacy_id`, `created_by`)
+INSERT INTO `event_type_lookup`
+(`code`, `name`, `display_order`, `start_date`, `end_date`, `mot1_legacy_id`, `last_updated_by`, `last_updated_on`, `created_by`)
 VALUES
-    ('UAE', 'Update AE', @displayOrder + 1, '1900-01-01', null, null, @createdBy);
+('UAE', 'Update AE', @displayOrder + 1, '1900-01-01', null, null, @createdBy, CURRENT_TIMESTAMP(6), @createdBy);
 
-INSERT INTO `permission` (`code`, `name`, `created_by`)
+INSERT INTO `permission` (`code`, `name`, `last_updated_by`, `last_updated_on`, `created_by`)
 VALUES
-  ('AUTHORISED-EXAMINER-STATUS-UPDATE', 'Update status for an Authorised Examiner', @createdBy);
+  ('AUTHORISED-EXAMINER-STATUS-UPDATE', 'Update status for an Authorised Examiner', @createdBy, CURRENT_TIMESTAMP(6), @createdBy);
 
 SET @permission = (SELECT `id` FROM `permission` WHERE `code` = 'AUTHORISED-EXAMINER-STATUS-UPDATE');
 SET @ao1_role = (SELECT `id` FROM `role` WHERE `code` = 'DVSA-AREA-OFFICE-1');
 
 INSERT INTO
-  `role_permission_map` (`role_id`, `permission_id`, `created_by`)
+  `role_permission_map` (`role_id`, `permission_id`, `last_updated_by`, `last_updated_on`, `created_by`)
 VALUES
-  (@ao1_role, @permission, @createdBy);
+  (@ao1_role, @permission, @createdBy, CURRENT_TIMESTAMP(6), @createdBy);
