b198f470f733eb860650e8d388169499fcc6b4b9
DVSA <dvsa@dvsa.gov.uk>
2015-09-03 11:16:21 +0100

    VM-11323: Update OtpService injection points to select adapter

---
+++ mot-api/module/DvsaAuthentication/test/DvsaAuthenticationTest/Factory/OtpServiceFactoryTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace DvsaAuthenticationTest\Factory;
+
+use Doctrine\ORM\EntityManager;
+use DvsaAuthentication\Factory\OtpServiceFactory;
+use DvsaAuthentication\Service\OtpService;
+use DvsaEntities\Repository\ConfigurationRepositoryInterface;
+use DvsaEntities\Repository\PersonRepository;
+use Zend\Authentication\AuthenticationService;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class OtpServiceFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    private $serviceLocator;
+
+    protected function setUp()
+    {
+        $repository = $this->getMockBuilder(PersonRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $entityManager = $this->getMockBuilder(EntityManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $entityManager->expects($this->any())
+            ->method('getRepository')
+            ->willReturn($repository);
+
+        $this->serviceLocator = $this->getServiceLocator([
+            EntityManager::class => $entityManager,
+            'DvsaAuthenticationService' => $this->getMock(AuthenticationService::class),
+            'ConfigurationRepository' => $this->getMock(ConfigurationRepositoryInterface::class),
+        ]);
+    }
+
+    public function testItIsAZendFactory()
+    {
+        $this->assertInstanceOf(FactoryInterface::class, new OtpServiceFactory());
+    }
+
+    public function testItCreatesAnOtpService()
+    {
+        $factory = new OtpServiceFactory();
+
+        $otpService = $factory->createService($this->serviceLocator);
+
+        $this->assertInstanceOf(OtpService::class, $otpService);
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getServiceLocator(array $services)
+    {
+        $serviceLocator = $this->getMock(ServiceLocatorInterface::class);
+
+        $serviceLocator->expects($this->any())
+            ->method('get')
+            ->with(call_user_func_array([$this, 'logicalOr'], array_keys($services)))
+            ->will($this->returnCallback(function ($serviceName) use ($services) {
+                return $services[$serviceName];
+            }));
+
+        return $serviceLocator;
+    }
+}
