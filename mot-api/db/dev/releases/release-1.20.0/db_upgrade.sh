896ac67b51139e8bec2f3aa04157c01ffd23adc4
DVSA <dvsa@dvsa.gov.uk>
2015-11-04 16:26:17 +0000

    VM-12003: Allow user with Tester Qualification status of initial training required to do a training test

--- mot-api/db/dev/releases/release-1.20.0/db_upgrade.sh
+++ mot-api/db/dev/releases/release-1.20.0/db_upgrade.sh
@@ -4,12 +4,13 @@ version=1.20.0
 db_name=mot_v195_rel4
 
 # add files here in intended run order for release
-upgradefiles+=('2015-10-30-VM-12310-create-db-version-table-STORY.sql')
-upgradefiles+=('2015-10-27-VM-12321-view-trade-roles-page-STORY.sql')
+upgradefiles+=('2015-10-30-VM-12310-create-db-version-table-STORY.sql');
+upgradefiles+=('2015-10-27-VM-12321-view-trade-roles-page-STORY.sql');
 upgradefiles+=('2015-10-30-VM-12280-create-and-assign-VTS-test-logs-permission.sql')
 upgradefiles+=('2015-10-27-VM-12328-certificate-replacement-additional-columns-only.sql');
 upgradefiles+=('2015-10-30-VM-12328-replacement-certificate-draft-and-permissions-update.sql');
 upgradefiles+=('2015-10-27-VM-12328-populate-mismatch-and-pass-flags.sql');
+upgradefiles+=('2015-10-30-VM-12003-allow-user-with-tester-qualification-status-of-initial-training-required-to-do-a-training-test-STORY.sql');
 
 for sqlscript in ${upgradefiles[@]}
 do
