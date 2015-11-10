dfb8daf7a9242da9f73cb0862385d0405a6f886d
DVSA <dvsa@dvsa.gov.uk>
2015-11-10 14:19:36 +0000

    VM-12209: Unable to buy slots if AE slot count is negative

--- mot-api/db/dev/update/1190/2015-10-16-VM-12209-buy-slots-when-negative-balance-STORY.sql
+++ mot-api/db/dev/update/1190/2015-10-16-VM-12209-buy-slots-when-negative-balance-STORY.sql
@@ -2,4 +2,4 @@ ALTER TABLE `test_slot_transaction`
 MODIFY `slots_after` INT(10) signed NOT NULL DEFAULT 0;
 
 ALTER TABLE `test_slot_transaction_hist`
-MODIFY `slots_after` INT(10) signed NOT NULL DEFAULT 0;
+MODIFY `slots_after` INT(10) signed DEFAULT NULL;
