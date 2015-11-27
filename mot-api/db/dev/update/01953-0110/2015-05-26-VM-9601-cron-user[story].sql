6bf9de0d44f58d13d2269fb96275b0264d3ee948
DVSA <dvsa@dvsa.gov.uk>
2015-11-27 11:01:39 +0000

    Added relevant id and name fields for cron and system users

--- mot-api/db/dev/update/01953-0110/2015-05-26-VM-9601-cron-user[story].sql
+++ mot-api/db/dev/update/01953-0110/2015-05-26-VM-9601-cron-user[story].sql
@@ -25,7 +25,7 @@
 -- FOR PRODUCTION:
 INSERT INTO `person` (`id`, `username`, `pin`, `user_reference`, `mot_one_user_id`, `title_id`, `first_name`, `middle_name`, `family_name`, `driving_licence_id`, `gender_id`, `date_of_birth`, `disability`, `demo_test_tester_status_id`, `otp_failed_attempts`, `is_account_claim_required`, `transition_status_id`, `mot1_userid`, `mot1_current_smartcard_id`, `2fa_token_id`, `2fa_token_sent_on`, `details_confirmed_on`, `first_training_test_done_on`, `first_live_test_done_on`, `is_deceased`, `deceased_on`, `mot1_details_updated_on`, `mot1_legacy_id`, `created_by`, `created_on`, `last_updated_by`, `last_updated_on`, `version`, `batch_number`)
 VALUES
-(4, 'cron-job', '', NULL, NULL, NULL, ' ', ' ', ' ', NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, 1, '2014-12-05 11:56:51.902817', NULL, NULL, 2, 0);
+(4, 'cron-job', '', NULL, NULL, NULL, 123456, 'cron_first', 'cron_middle', NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, 1, '2014-12-05 11:56:51.902817', NULL, NULL, 2, 0);
 
 SET @cron_old_id = (SELECT `id` FROM `person` WHERE `username`='sn-cron-job');
 
