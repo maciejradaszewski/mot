2a4e6d82c3a7a966d5a4be09940b82ad7d9407a7
DVSA <dvsa@dvsa.gov.uk>
2016-02-05 08:17:33 +0000

    Removing Grunt Migration Code

--- mot-static/src/grunt/config/vagrant_config.js
+++ mot-static/src/grunt/config/vagrant_config.js
@@ -1,95 +1,54 @@
-module.exports = function(grunt, config) {
+module.exports = function (grunt, config) {
 
     if (config.environment === config.ENV_DEVELOPMENT) {
 
-        var execSync  = require('exec-sync');
-        var util      = require('util');
-        var infFolder = (config.legacy !== true) ?
-            process.env['dvsa_workspace'] + '/mot-vagrant' : process.env['dev_workspace'] + '/../infrastructure';
+        var execSync = require('exec-sync');
+        var util = require('util');
 
-        var getSSHPort = function(vagrantHost) {
-            var port = execSync(util.format('cd %s && vagrant ssh-config ' + vagrantHost + ' | grep Port | awk \'{print $2}\'', infFolder));
-
-            return port ? parseInt(port, 10) : 22;
-        };
+        var infFolder = process.env['dvsa_workspace'] + '/mot-vagrant';
 
         // Wrapper: If a VM is unresponsive, answers "" instead of blowing out grunt.
-        var getKeyCmd = function(cmd) {
+        var getKeyCmd = function (cmd, name) {
+            var cacheFile = './.vagrant_ssh_config_cache/vagrant-ssh-config-' + name + '.local.cached';
+            if (grunt.file.exists(cacheFile)) {
+                return grunt.file.readJSON(cacheFile).privateKey;
+            }
             var file = execSync(util.format(cmd, infFolder));
-
-            return (file.length > 0) ? grunt.file.read(file.replace(/\"/g,'')) : '';
+            var result = (file.length > 0) ? grunt.file.read(file) : '';
+            grunt.file.write(cacheFile, JSON.stringify({privateKey: result}));
+            return result;
         };
 
-        if (config.legacy === false) {
+        grunt.config('service_config', {
+            //workspace:  '/home/vagrant/mot',
+            //phpRootDir: '/opt/rh/php55/root',
+            httpdServiceName: 'httpd24-httpd'
+        });
 
-            getKeyCmd = function(cmd, name) {
-                var cacheFile = './.vagrant_ssh_config_cache/vagrant-ssh-config-' + name + '.local.cached';
-                if(grunt.file.exists(cacheFile)) {
-                    return grunt.file.readJSON(cacheFile).privateKey;
-                }
-                var file = execSync(util.format(cmd, infFolder));
-                var result = (file.length > 0) ? grunt.file.read(file) : '';
-                grunt.file.write(cacheFile, JSON.stringify({ privateKey : result}));
-                return result;
-            };
+        // dev (Frontend, OpenDJ, Jasper, MySQL, Distauth)
+        grunt.config('dev_config', {
+            host: '10.10.10.30',
+            port: 22,
+            username: 'vagrant',
+            privateKey: getKeyCmd('cd %s && vagrant ssh-config dev | grep IdentityFile | awk \'{print $2}\'', 'dev'),
+            workspace: '/home/vagrant/mot'
+        });
+        // dev2 (API, OpenAM)
+        grunt.config('dev2_config', {
+            host: '10.10.10.50',
+            port: 22,
+            username: 'vagrant',
+            privateKey: getKeyCmd('cd %s && vagrant ssh-config dev2 | grep IdentityFile | awk \'{print $2}\'', 'dev2'),
+            workspace: '/home/vagrant/mot',
+            phpRootDir: '/opt/rh/php55/root'
+        });
 
-            grunt.config('service_config', {
-                //workspace:  '/home/vagrant/mot',
-                //phpRootDir: '/opt/rh/php55/root',
-                httpdServiceName: 'httpd24-httpd'
-            });
+        // dev2 (API, OpenAM)
+        grunt.config('vagrant_config', grunt.config.get('dev2_config'));
+        // dev (Frontend, OpenDJ, Jasper, MySQL)
+        grunt.config('jasper_config', grunt.config.get('dev_config'));
+        // dev2 (API, OpenAM)
+        grunt.config('devopenam_config', grunt.config.get('dev2_config'));
 
-            // dev (Frontend, OpenDJ, Jasper, MySQL, Distauth)
-            grunt.config('dev_config', {
-                host:       '10.10.10.30',
-                port:       22,
-                username:   'vagrant',
-                privateKey: getKeyCmd('cd %s && vagrant ssh-config dev | grep IdentityFile | awk \'{print $2}\'', 'dev'),
-                workspace:  '/home/vagrant/mot'
-            });
-            // dev2 (API, OpenAM)
-            grunt.config('dev2_config', {
-                host:       '10.10.10.50',
-                port:       22,
-                username:   'vagrant',
-                privateKey: getKeyCmd('cd %s && vagrant ssh-config dev2 | grep IdentityFile | awk \'{print $2}\'', 'dev2'),
-                workspace:  '/home/vagrant/mot',
-                phpRootDir: '/opt/rh/php55/root'
-            });
-
-            // dev2 (API, OpenAM)
-            grunt.config('vagrant_config', grunt.config.get('dev2_config'));
-            // dev (Frontend, OpenDJ, Jasper, MySQL, Distauth)
-            grunt.config('jasper_config', grunt.config.get('dev_config'));
-            // dev2 (API, OpenAM)
-            grunt.config('devopenam_config', grunt.config.get('dev2_config'));
-        } else {
-            grunt.config('service_config', {
-                //workspace:  '/workspace',
-                //phpRootDir: '',
-                httpdServiceName: 'httpd'
-            });
-
-            grunt.config('vagrant_config', {
-                host:       '192.168.149.30',
-                port:       22,
-                username:   'vagrant',
-                privateKey: getKeyCmd('cd %s && vagrant ssh-config lamp-mot | grep IdentityFile | awk \'{print $2}\''),
-                workspace:  '/workspace',
-                phpRootDir: '',
-            });
-            grunt.config('jasper_config', {
-                host:       '192.168.149.33',
-                port:       22,
-                username:   'vagrant',
-                privateKey: getKeyCmd('cd %s && vagrant ssh-config jasper2 | grep IdentityFile | awk \'{print $2}\'')
-            });
-            grunt.config('devopenam_config', {
-                host:       '192.168.149.9',
-                port:       22,
-                username:   'vagrant',
-                privateKey: getKeyCmd('cd %s && vagrant ssh-config devopenam | grep IdentityFile | awk \'{print $2}\'')
-            });
-        }
     }
 };
--- mot-static/src/grunt/tasks/apache.js
+++ mot-static/src/grunt/tasks/apache.js
@@ -6,7 +6,7 @@
  */
 module.exports = function(grunt, config) {
     if (config.environment === config.ENV_DEVELOPMENT) {
-        grunt.registerTask('apache:restart', 'LEGACY. Restart the apache server in the VM', 'sshexec:apache_restart');
+        grunt.registerTask('apache:restart', 'See apache:restart:all', 'apache:restart:all');
         grunt.registerTask('apache:restart:frontend', 'Restart the apache server on the dev VM', 'sshexec:apache_restart_dev');
         grunt.registerTask('apache:restart:api', 'Restart the apache server on the dev2 VM', 'sshexec:apache_restart_dev2');
         grunt.registerTask('apache:restart:all', 'Restart the apache server on both dev and dev2 VMs', [
