4f154ddd34d5f7f596d11a9361b0920401cf18a8
DVSA <dvsa@dvsa.gov.uk>
2016-09-14 09:32:43 +0000

    BL-2980: Unlink static and anonymised data

--- mot-api/db/create_db_with_test_data.sh
+++ mot-api/db/create_db_with_test_data.sh
@@ -24,6 +24,8 @@ echo $(date) Loading test data into mot2 on $MyHOST as $MyUSER
 mysql -u $MyUSER -p$MyPASS -h $MyHOST -D mot2 < ./populate_db.sql_temp
 rm ./populate_db.sql_temp
 
+echo $(date) Loading static data
+mysql -u $MyUSER -p$MyPASS -h $MyHOST -D mot2 -f < ./dev/populate/mot2_static_data.sql
 echo $(date) Loading anonymised data set
 mysql -u $MyUSER -p$MyPASS -h $MyHOST -D mot2 -f < ./dev/populate/mot2_data_10k.sql
 
