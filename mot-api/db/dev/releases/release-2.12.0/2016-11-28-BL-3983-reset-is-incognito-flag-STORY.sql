6772b7a3c458cc402f81c5feb232699ccd11749c
DVSA <dvsa@dvsa.gov.uk>
2016-12-05 10:38:39 +0000

    BL-3983: Reset is_incognito flag for all vehicles in Production

--- mot-api/db/dev/releases/release-2.12.0/2016-11-28-BL-3983-reset-is-incognito-flag-STORY.sql
+++ mot-api/db/dev/releases/release-2.12.0/2016-11-28-BL-3983-reset-is-incognito-flag-STORY.sql
@@ -1,4 +1,6 @@
 -- Reset flag to false before mystery shopper epic will be turned ON on PROD
+SET @app_user_id = (SELECT `id` FROM `person` WHERE `username` = 'static data' OR `user_reference` = 'Static Data');
+
 UPDATE vehicle
 SET    is_incognito = FALSE
 WHERE  is_incognito = TRUE;
