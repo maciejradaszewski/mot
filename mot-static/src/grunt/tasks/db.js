763e5df4dd4dd962e336cdc62f71e95148a1f786
DVSA <dvsa@dvsa.gov.uk>
2017-03-23 16:29:58 +0000

    BL-4357: Redirect Vehicle Examiner to new homepage

--- mot-static/src/grunt/tasks/db.js
+++ mot-static/src/grunt/tasks/db.js
@@ -5,6 +5,8 @@
  * @param config
  */
 module.exports = function(grunt, config) {
-        grunt.registerTask('db:reset', 'Reset the database on the VM with the small data set', 'sshexec:reset_database');
+        grunt.registerTask('db:reset', 'Reset the database with the small data set', 'sshexec:reset_database');
+        grunt.registerTask('db:reset:anonymised', 'Reset the database with the anonymised data set', 'shell:reset_database_anonymised');
+        grunt.registerTask('db:reset:10k', 'Reset the database with the 10k data set', 'shell:reset_database_10k');
         grunt.registerTask('db:repair', 'Repair a broken mysql proc table in the VM', 'sshexec:mysql_proc_fix');
 };
