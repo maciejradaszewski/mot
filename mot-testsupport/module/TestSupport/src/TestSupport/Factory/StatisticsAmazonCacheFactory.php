c7f2a4dc162520a4f0a5f771dda812b74e74c288
DVSA <dvsa@dvsa.gov.uk>
2017-05-10 14:13:52 +0000

    Apply php check style to mot repo

--- mot-testsupport/module/TestSupport/src/TestSupport/Factory/StatisticsAmazonCacheFactory.php
+++ mot-testsupport/module/TestSupport/src/TestSupport/Factory/StatisticsAmazonCacheFactory.php
@@ -13,14 +13,21 @@ class StatisticsAmazonCacheFactory implements FactoryInterface
     {
         $awsConfig = $serviceLocator->get('Config')['aws']['statisticsAmazonStorage'];
 
-        $s3Client = new S3Client([
-            'credentials' => [
-                'key' => $awsConfig['accessKeyId'],
-                'secret' => $awsConfig['secretKey'],
-            ],
-            'version' => 'latest',
-            'region' => $awsConfig['region'],
-        ]);
+        $s3ClientArgs = [
+             'version' => 'latest',
+             'region' => $awsConfig['region'],
+        ];
+
+        //If "accessKeyId" and "secretKey" are not defined then fallback to IAM roles.
+        if ($this->keyExists('accessKeyId', $awsConfig) ||
+           $this->keyExists('secretKey', $awsConfig)) {
+           $s3ClientArgs['credentials'] = [
+               'key' => $awsConfig['accessKeyId'],
+              'secret' => $awsConfig['secretKey'],
+           ];
+        }
+
+        $s3Client = new S3Client($s3ClientArgs);
 
         $bucket = $awsConfig['bucket'];
 
@@ -32,4 +39,9 @@ class StatisticsAmazonCacheFactory implements FactoryInterface
             $env
         );
     }
+
+    private function keyExists($key, $config) {
+      return array_key_exists($key, $config) && $config[$key] != '';
+    }
+
 }
